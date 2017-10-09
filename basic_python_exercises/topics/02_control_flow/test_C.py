0efc7afa350c0b6d4e779e018d473c08b6bef45a
James Collins <collijk@uw.edu>
2017-10-09 13:25:17 -0700

    Finish test for control flow exercise C

--- basic_python_exercises/topics/02_control_flow/test_C.py
+++ basic_python_exercises/topics/02_control_flow/test_C.py
@@ -1,8 +1,8 @@
 import random
-import math
+from itertools import combinations
 from .C import possible_ingredients, determine_pizza_choices
 
-# TODO: Write this test
+
 def test_determine_pizza_choices():
     for _ in range(100):
         num_people = random.randint(2, 5)
@@ -23,21 +23,19 @@ def test_determine_pizza_choices():
 
         flattened_dislikes = [i for person_dislikes in dislikes for i in person_dislikes]
         no_good = set([i for i in flattened_dislikes if isinstance(i, str)])
-        no_good_combos_start = [i for i in flattened_dislikes if isinstance(i, list)]
-
-        # Remove duplicates and unavailable ingredient combinations
-        no_good_combos = []
-        for combo in no_good_combos_start:
-            combo_contains_unavailable_ingredients = [ingredient for ingredient in combo if ingredient in no_good]
-            if combo not in no_good_combos and not combo_contains_unavailable_ingredients:
-                no_good_combos.append(combo)
-
+        no_good_combos = [i for i in flattened_dislikes if isinstance(i, list)]
         final_ingredients = set(available_ingredients).difference(set(no_good))
-        num_combos = math.factorial(len(final_ingredients))
-        num_bad_combos = 0
-        for combo in no_good_combos:
-            # FIXME: This overcounts the bad combos
-            num_bad_combos += math.factorial(len(final_ingredients) - len(combo)) + 1
+
+        count = 0
+        for i in range(1, len(final_ingredients)+1):
+            for p in combinations(final_ingredients, i):
+                pizza_good = True
+                for c in no_good_combos:
+                    if c in p:
+                        pizza_good = False
+                        break
+                if pizza_good:
+                    count += 1
 
         func_out = determine_pizza_choices(available_ingredients, dislikes)
-        assert func_out == num_combos - num_bad_combos
+        assert func_out == count
