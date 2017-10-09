7a4c39ecc41c4235f73ed2148de8579c8b136fe1
James Collins <collijk@uw.edu>
2017-10-09 08:11:20 -0700

    Added class exercises

---
+++ basic_python_exercises/topics/01_basic_syntax/01_arithmetic/test_class_exercise.py
@@ -0,0 +1,46 @@
+import random
+
+import pytest
+
+from .class_excercise import add, subtract, multiply, divide
+
+
+def test_add():
+    maximum = 5e15
+    minimum = -5e15
+    for i in range(1000):
+        a = random.uniform(minimum, maximum)
+        b = random.uniform(minimum, maximum)
+        assert add(a, b) == a + b
+
+
+def test_subtract():
+    maximum = 5e15
+    minimum = -5e15
+    for i in range(1000):
+        a = random.uniform(minimum, maximum)
+        b = random.uniform(minimum, maximum)
+        assert subtract(a, b) == a - b
+
+
+def test_multiply():
+    maximum = 5e15
+    minimum = -5e15
+    for i in range(1000):
+        a = random.uniform(minimum, maximum)
+        b = random.uniform(minimum, maximum)
+        assert multiply(a, b) == a * b
+
+
+def test_divide():
+    maximum = 5e15
+    minimum = -5e15
+    for i in range(1000):
+        a = random.uniform(minimum, maximum)
+        b = random.uniform(minimum, maximum)
+        if b == 0:  # Minuscule but non-zero probability.
+            continue
+        assert divide(a, b) == a / b
+    a = random.uniform(minimum, maximum)
+    with pytest.raises(AssertionError):
+        divide(a, 0)
