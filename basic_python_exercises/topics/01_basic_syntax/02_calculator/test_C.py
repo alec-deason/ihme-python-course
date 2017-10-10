0f8388d5359d12f0915b5cb0e01d82509a0f056a
Alec Deason <alecwd@uw.edu>
2017-10-09 18:01:33 -0700

    Fix prime factors test

--- basic_python_exercises/topics/01_basic_syntax/02_calculator/test_C.py
+++ basic_python_exercises/topics/01_basic_syntax/02_calculator/test_C.py
@@ -1,20 +1,23 @@
 from itertools import product
+import math
 
 from C import calculator
 
 def test_sum_of_prime_factors():
     for a in range(0, 100):
+        primes = []
         prime_factors = []
-        for i in range(2, a+1):
-            if a % i == 0:
-                is_prime = True
-                for p in prime_factors:
-                    if i % p == 0:
-                        is_prime = False
-                        break
-                if is_prime:
+        for i in range(2, int(math.sqrt(a))+1):
+            is_prime = True
+            for p in primes:
+                if i % p == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                primes.append(i)
+                if a % i == 0:
                     prime_factors.append(i)
-        assert calculator('sum_of_prime_factors', [a]) == sum(prime_factors) + 1
+        assert calculator('sum_of_prime_factors', [a]) == sum(prime_factors)
 
 
 def test_add():
